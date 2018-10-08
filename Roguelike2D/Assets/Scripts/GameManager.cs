5d1c1dfd742585e346214621a906e962ad26e534
matias-milletari <matias.milletari@gmail.com>
2018-10-08 14:16:57 -0300

    Agregados scripts para el manejo del personaje y enemigos

--- Roguelike2D/Assets/Scripts/GameManager.cs
+++ Roguelike2D/Assets/Scripts/GameManager.cs
@@ -1,11 +1,19 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager instance = null;
+    public static GameManager instance;
+    public int foodPoints = 100;
+    public float turnDelay = .1f;
+    [HideInInspector] public bool playersTurn = true;
 
     private BoardManager boardManager;
     private int level = 3;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
+
 
     private void Awake()
     {
@@ -20,6 +28,8 @@ public class GameManager : MonoBehaviour
 
         DontDestroyOnLoad(gameObject);
 
+        enemies = new List<Enemy>();
+
         boardManager = GetComponent<BoardManager>();
 
         InitializeGame();
@@ -27,11 +37,47 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
+        if (playersTurn || enemiesMoving)
+        {
+            return;
+        }
 
+        StartCoroutine(MoveEnemies());
+    }
+
+    public void AddEnemyToList(Enemy enemy)
+    {
+        enemies.Add(enemy);
+    }
+
+    public void GameOver()
+    {
+        enabled = false;
     }
 
     private void InitializeGame()
     {
+        enemies.Clear();
         boardManager.SetupScene(level);
+    }
+
+    private IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+
+        if (enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        foreach (var enemy in enemies)
+        {
+            enemy.MoveEnemy();
+            yield return new WaitForSeconds(enemy.moveTime);
+        }
+
+        playersTurn = true;
+        enemiesMoving = false;
     }
 }
---
+++ Roguelike2D/Assets/Scripts/Player.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MovingObject
+{
+    public int wallDamage = 1;
+    public int pointsPerFood = 10;
+    public int pointsPerSoda = 20;
+    public float restarLevelDelay = 1f;
+
+    private Animator animator;
+    private int food;
+
+    protected override void Start()
+    {
+        animator = GetComponent<Animator>();
+        food = GameManager.instance.foodPoints;
+
+        base.Start();
+    }
+
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    {
+        food--;
+
+        base.AttemptMove<T>(xDir, yDir);
+
+        RaycastHit2D hit;
+
+        CheckIfGameOver();
+
+        GameManager.instance.playersTurn = false;
+    }
+
+    protected override void OnCantMove<T>(T component)
+    {
+        var hitWall = component as Wall;
+
+        hitWall.DamageWall(wallDamage);
+
+        animator.SetTrigger("Chop");
+    }
+
+    private void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void OnDisable()
+    {
+        GameManager.instance.foodPoints = food;
+    }
+
+    private void CheckIfGameOver()
+    {
+        if (food <= 0)
+        {
+            GameManager.instance.GameOver();
+        }
+    }
+
+    private void Update()
+    {
+        if (!GameManager.instance.playersTurn) return;
+
+        var horizontal = 0;
+        var vertical = 0;
+
+        horizontal = (int)Input.GetAxisRaw("Horizontal");
+        vertical = (int)Input.GetAxisRaw("Vertical");
+
+        if (horizontal != 0)
+        {
+            vertical = 0;
+        }
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            AttemptMove<Wall>(horizontal, vertical);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Exit")
+        {
+            Invoke("Restart", restarLevelDelay);
+        }
+        else if(other.tag == "Food")
+        {
+            food += pointsPerFood;
+            other.gameObject.SetActive(false);
+        }
+        else if (other.tag == "Soda")
+        {
+            food += pointsPerSoda;
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    public void LoseFood(int foodLost)
+    {
+        animator.SetTrigger("Hit");
+        food = -foodLost;
+        CheckIfGameOver();
+    }
+}
